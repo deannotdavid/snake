86900366301718c60e52c6f47c398afbeee1b5eb
Dean David <dean.not.david@gmail.com>
2022-02-02 14:12:06 +0000

    Added leaderboard screen to game

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import random
 
 import pygame
 
+import leaderboard as lb
 from utils import *
 from board import Board
-from snake import Snake
+from snake import Snake, LoopSnake
 
 from constants.colors import *
 from constants.dimensions import WIDTH, HEIGHT
@@ -23,20 +24,64 @@ class Game:
 		self.down = pygame.K_DOWN, pygame.K_s
 
 		# fonts
-		font_path = os.path.join("data", "fonts", "nokiafc22.ttf")
-		self.score_font = pygame.font.Font(font_path, 256)
+		nokia_path = os.path.join("data", "fonts", "nokiafc22.ttf")
+		arcade_path = os.path.join("data", "fonts", "PressStart2P.ttf")
+		self.score_font = pygame.font.Font(nokia_path, 256)
+		self.header_font = pygame.font.Font(arcade_path, 56)
+		self.body_font = pygame.font.Font(arcade_path, 32)
+
+	def leaderboard(self):
+		# gets top 10 scores from the leaderboard database
+		top_scores = lb.get_top(10)
+		# adds blank spots if top 10 is not full
+		while len(top_scores) < 10:
+			top_scores.append(('___', 0))
+
+		# gets the center co-ordinates of self.win
+		center_x, center_y = self.win.get_rect().center
+
+		clock = pygame.time.Clock()
+		
+		# creates surfaces for leaderboard to be drawn inside the leaderboard
+		header = "HIGH SCORES"
+		header_surf = self.header_font.render(header, False, WHITE)
+
+		leaderboard_lines = list()
+		for place, score in enumerate(top_scores):
+			place_str = str(place + 1)
+			name, points = score
+			line = f"{place_str.rjust(2)} {name.center(9)} {str(points).rjust(3, '0')}"
+			line_surf = self.body_font.render(line, False, WHITE)
+			line_center = center_x - (line_surf.get_width()/2)
+			y = 97 + (place * 50)
+			leaderboard_lines.append((line_surf, line_center, y))
+
+		while True:
+			clock.tick(self.FPS)
+			for e in pygame.event.get():
+				if e.type == pygame.QUIT:
+					pygame.quit()
+					quit()
+
+			self.win.fill(BLACK)
+
+			# blit header
+			self.win.blit(header_surf, (center_x - header_surf.get_width()/2, 20))
+
+			# blit leaderboard
+			for surf, x, y in leaderboard_lines:
+				self.win.blit(surf, (x, y))
+
+			pygame.display.update()
 
 	def play_game(self):
-		print("new")
-
 		score = 0
-		snake = Snake()
+		snake = LoopSnake()
 
 		board = Board()
 		board.generate_board(snake.positions)
 
 		clock = pygame.time.Clock()
-
 
 		move_event = pygame.USEREVENT + 1
 
@@ -52,8 +97,6 @@ class Game:
 				if e.type == pygame.QUIT:
 					quit_program()
 				if e.type == pygame.KEYDOWN:
-					if e.key == pygame.K_SPACE:
-						board.generate_board()
 					if e.key == pygame.K_ESCAPE:
 						quit_program()
 					if dir_changed == False:
@@ -76,14 +119,7 @@ class Game:
 						score += 1
 					if snake.check_collisions():
 						print(score)
-						# score_surf = self.score_font.render(str(score), False,
-						# 	WHITE)
-						# score_rect = score_surf.get_rect(center=self.win.get_rect().center)
-						# self.win.blit(score_surf, score_rect)
-						# pygame.display.update()
-						# pygame.time.delay(700)
-						self.game_over(score)
-						return
+						return score
 
 			self.win.fill(BLACK)
 			board.draw(self.win)
@@ -96,27 +132,28 @@ class Game:
 		score_rect = score_surf.get_rect(center=self.win.get_rect().center)
 
 		clock = pygame.time.Clock()
+		self.win.blit(score_surf, score_rect)
+		pygame.display.update()
+
 
 		while True:
 			clock.tick(self.FPS)
-			self.win.blit(score_surf, score_rect)
 			for e in pygame.event.get():
 				if e.type == pygame.QUIT:
 					quit_program()
 				if e.type == pygame.KEYDOWN:
-					if e.key == pygame.K_ESCAPE:
-						quit_program()
 					if e.key == pygame.K_SPACE:
+						print("hi")
 						return
-			pygame.display.update()
 
 
 def main():
 	pygame.init()
 	g = Game()
-	while True:
-		g.play_game()
-
+	g.leaderboard()
+	# while True:
+	# 	score = g.play_game()
+	# 	g.game_over(score)
 
 if __name__ == '__main__':
 	main()
